93ff762ee53058a607cf3feabb14a46a0dfff11f
BryanErnandes <Bryandeduartina1999@outlook.com>
2023-04-04 12:54:30 -0300

    atualizacao

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -31,30 +31,30 @@ export default function Login() {
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <Container>
-            <ContainerBanner>
-                <Banner
-                    resizeMode="stretch"
-                    source={require('../../../assets/logo.png')} />
-            </ContainerBanner>
+                <ContainerBanner>
+                    <Banner
+                        resizeMode="stretch"
+                        source={require('../../../assets/logo.png')} />
+                </ContainerBanner>
 
-            <Form>
-                <Title>Nome de usuario</Title>
-                <Input placeholder='Nome de Usuario'
-                    placeholderTextColor="#000000"
-                    value={email} onChangeText={setEmail} />
-                <Title>Sua senha</Title>
-                <Input placeholder='Senha'
-                    placeholderTextColor="#000000"
-                    value={password} onChangeText={setPassword}
-                    secureTextEntry={true} />
+                <Form>
+                    <Title>Nome de usuario</Title>
+                    <Input placeholder='Nome de Usuario'
+                        placeholderTextColor="#000000"
+                        value={email} onChangeText={setEmail} />
+                    <Title>Sua senha</Title>
+                    <Input placeholder='Senha'
+                        placeholderTextColor="#000000"
+                        value={password} onChangeText={setPassword}
+                        secureTextEntry={true} />
 
-                <Botao activeOpacity={0.5}>
-                    <TituloBotao onPress={() => loginHandle()}>Entra</TituloBotao>
-    </Botao>
-                {/*<Botao3 activeOpacity={0.5}>
+                    <Botao activeOpacity={0.5}>
+                        <TituloBotao onPress={() => loginHandle()}>Entra</TituloBotao>
+                    </Botao>
+                    {/*<Botao3 activeOpacity={0.5}>
                     <TituloBotao3>Não possuir uma conta? Cadastre-se</TituloBotao3>
     </Botao3>*/}
-               {/* <Sociais>
+                    {/* <Sociais>
                     <Facebook>
                         <Entypo name="facebook" size={55} color="#0D2CDB" />
                     </Facebook>
@@ -65,10 +65,10 @@ export default function Login() {
                         <AntDesign name="google" size={55} color="#FA1D00" />
                     </Google>
     </Sociais>*/}
-            </Form>
-        </Container>
+                </Form>
+            </Container>
         </TouchableWithoutFeedback>
-            
-        
+
+
     )
 }
